95c3752f56b6457e32ed5d36d9096e7a6d7540f7
Ortega1988 <neo-yar@bk.ru>
2025-06-21 16:04:52 +0300

    Тестовый бот

--- bot.py
+++ bot.py
@@ -1,1 +1,22 @@
-print('Bot положен')
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token="8056672850:AAEIwgFTrT-_LDg4ds8PYRtjsHHLC70CYdw")
+dp = Dispatcher()
+
+
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Hello!")
+
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
