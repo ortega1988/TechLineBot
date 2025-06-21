522a874ccaf4f8f01477e5226c6b21e07738b6ea
Ortega1988 <neo-yar@bk.ru>
2025-06-21 16:53:44 +0300

    123

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ dp = Dispatcher()
 
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer("Привет Детям223332222!")
+    await message.answer("Привет Детям52!")
 
 
 async def main():
